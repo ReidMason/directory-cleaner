c5255949d7a6d93f352cee87ccb465df2ba7e6fd
Reid Mason <maddogshain132@gmail.com>
2023-02-08 00:48:59 +0000

    Refactored to explore structs

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,89 @@
-use std::{
-    fs::{self, DirEntry},
-    io,
-};
+mod test {
+    use std::{
+        fs::{self, DirEntry},
+        io,
+    };
 
-fn main() {
-    let removed_dirs = clean_dir("./testdata/testdir").expect("Unable to process directory");
-    println!("Removed {removed_dirs} directories");
-}
-
-fn clean_dir(filepath: &str) -> Result<i32, io::Error> {
-    let dirs_to_remove = ["node_modules", "__pycache__", "venv"];
-    let paths = fs::read_dir(filepath)?;
-    let mut dirs_removed = 0;
-
-    for path in paths {
-        let file = path?;
-
-        let filename = file.file_name();
-        let filename = match filename.to_str() {
-            Some(filename) => filename,
-            None => continue,
-        };
-
-        if dirs_to_remove.contains(&filename) {
-            if delete_directory(file) {
-                dirs_removed += 1;
-            }
-            continue;
-        }
-
-        if file.metadata()?.is_dir() {
-            match file.path().to_str() {
-                Some(filepath) => dirs_removed += clean_dir(filepath)?,
-                None => continue,
-            };
-        }
+    pub struct DirectoryCleaner {
+        dirs_to_remove: Vec<String>,
+        pub dirs_removed: i32,
     }
 
-    Ok(dirs_removed)
+    impl DirectoryCleaner {
+        pub fn new(dirs_to_remove: Vec<String>) -> Self {
+            Self {
+                dirs_to_remove,
+                dirs_removed: 0,
+            }
+        }
+
+        pub fn clean_dir(&mut self, filepath: String) -> Result<(), io::Error> {
+            for path in fs::read_dir(filepath)? {
+                let file = path?;
+
+                if self.delete_directory(&file) {
+                    continue;
+                }
+
+                self.try_recurse_clean_dir(file)?;
+            }
+            Ok(())
+        }
+
+        fn try_recurse_clean_dir(&mut self, file: DirEntry) -> Result<(), io::Error> {
+            if !file.metadata()?.is_dir() {
+                return Ok(());
+            }
+
+            match file.path().into_os_string().into_string() {
+                Ok(filepath) => self.clean_dir(filepath)?,
+                Err(_) => (),
+            };
+
+            Ok(())
+        }
+
+        fn get_filename(file: &DirEntry) -> String {
+            let filename = file.file_name();
+            match filename.into_string() {
+                Ok(x) => return x,
+                Err(_) => return String::new(),
+            };
+        }
+
+        fn delete_directory(&mut self, file: &DirEntry) -> bool {
+            let filename = DirectoryCleaner::get_filename(file);
+            if filename.is_empty() || !self.dirs_to_remove.contains(&filename) {
+                return false;
+            }
+
+            match fs::remove_dir_all(file.path()) {
+                Ok(_) => {
+                    println!("Removed directory: {:?}", file.path());
+                    self.dirs_removed += 1;
+                    true
+                }
+                Err(_) => {
+                    println!("Failed to remove directory: '{:?}'", file.path());
+                    false
+                }
+            }
+        }
+    }
 }
 
-fn delete_directory(file: DirEntry) -> bool {
-    match fs::remove_dir_all(file.path()) {
-        Ok(_) => {
-            println!("Removed directory: {:?}", file.path());
-            true
-        }
-        Err(_) => {
-            println!("Failed to remove directory: '{:?}'", file.path());
-            false
-        }
+fn main() {
+    let directory = String::from("./testdata/testdir");
+    let dirs_to_remove = vec![
+        "node_modules".to_string(),
+        "__pycache__".to_string(),
+        "venv".to_string(),
+    ];
+
+    let mut directory_cleaner = test::DirectoryCleaner::new(dirs_to_remove);
+
+    match directory_cleaner.clean_dir(directory.clone()) {
+        Ok(_) => println!("Removed {} directories", directory_cleaner.dirs_removed),
+        Err(_) => println!("Unable to read directory: '{directory}'"),
     }
 }
